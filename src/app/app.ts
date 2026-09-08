c3c7b34907c12c762e2b035e776ad6d53e7e5c8c
vhmsouza <sap0000305@aluno.firjansenaisesi.com.br>
2026-09-08 16:38:28 -0300

    feat: adionei tela de cadastro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Cadastro } from './pages/cadastro/cadastro';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet,Header,Footer,Cadastro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
