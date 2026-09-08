fcee52a189a2f2ef9a259c2dae38ec75124a0ee8
Lara Kellen Simplicio de Freitas <simpliciolara444@gmail.com>
2026-09-08 15:58:18 -0300

    Atualização dos arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+
+    {
+        path:'home',
+        loadComponent:() => 
+        import ('./pages/home/home').then (m => m.Home)
+    },
+    {
+        path:'produtos',
+        loadComponent:() => 
+        import ('./pages/produtos/produtos').then (m => m.Produtos)
+    },
+    { 
+        path:'carrinho',
+        loadComponent:() => 
+        import ('./pages/carrinho/carrinho').then (m => m.Carrinho)
+    },
+    {
+        path:'favoritos',
+        loadComponent:() => 
+        import ('./pages/favoritos/favoritos').then (m => m.Favoritos)
+   },
+    {
+        path:'cadastro',
+        loadComponent:() => 
+        import('./pages/cadastro/cadastro').then (m => m.Cadastro)
+    }
+];
